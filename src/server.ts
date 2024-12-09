73e65239d361e721833e29fa59de706151b0d553
Raphael Gibson <raphaelgibson1998@gmail.com>
2024-12-09 17:29:03 -0300

    feat: remove user seed code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import cors from '@fastify/cors'
 import Fastify from 'fastify'
-import { prismaClient } from './repositories/prisma-client'
 import { authRoutes, musicsRoutes, praisesRoutes } from './routes'
 
 const fastify = Fastify({ logger: true })
@@ -14,21 +13,6 @@ const start = async () => {
   try {
     await fastify.listen({ port: 3000, host: '0.0.0.0' })
     console.log('Servidor rodando em http://0.0.0.0:3000')
-    const email = process.env.EMAIL as string
-    const password = process.env.HASHED_PASSWORD as string
-
-    if (!email || !password) {
-      throw new Error('Invalid environment variables. (needs email and password)')
-    }
-
-    await prismaClient.user.create({
-      data: {
-        email,
-        name: 'Hebert Freire',
-        password,
-        admin: true
-      }
-    })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
